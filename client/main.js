4eb95bc204fe0c2231f460127f0e2273a866a2e0
nodza <noel.hwande@gmail.com>
2016-09-08 08:58:52 -0500

    Created Mongo collection for Employees

--- client/main.js
+++ client/main.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-Meteor.startup(() => {
+const App = () => {
+    return (
+        <div>Hola Mundo</div>
+    );
+};
 
+Meteor.startup(() => {
+    ReactDOM.render(<App />, document.querySelector('.container'));
 });
