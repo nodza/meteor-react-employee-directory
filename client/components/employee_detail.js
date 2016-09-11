fdba8737662e8c1b05b7b776ed854a5fb39e99e5
nodza <noel.hwande@gmail.com>
2016-09-11 14:43:42 -0500

    Added employee detail

---
+++ client/components/employee_detail.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const EmployeeDetail = (props) => {
+    return (
+        <div>
+            Employee Detail
+        </div>
+    )
+
+};
+
+export default EmployeeDetail;
--- client/components/employee_list.js
+++ client/components/employee_list.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { createContainer } from 'meteor/react-meteor-data';
 import { Employees } from '../../imports/collections/employees';
+import EmployeeDetail from './employee_detail';
 
-const EmployeeList = () => {
+const EmployeeList = (props) => {
     return (
         <div>
             <div className="employee-list">
-                Employee List
+                {props.employees.map(employee => <EmployeeDetail />)}
             </div>
         </div>
     )
