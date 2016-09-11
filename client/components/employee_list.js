8adcbb5c6716701fcc38b3a678f14be9ad591f9f
nodza <noel.hwande@gmail.com>
2016-09-11 18:18:38 -0500

    Added flexbox styling and Load More functionality

--- client/components/employee_list.js
+++ client/components/employee_list.js
@@ -3,19 +3,28 @@ import { createContainer } from 'meteor/react-meteor-data';
 import { Employees } from '../../imports/collections/employees';
 import EmployeeDetail from './employee_detail';
 
+const PER_PAGE = 20;
+
 const EmployeeList = (props) => {
     return (
         <div>
             <div className="employee-list">
-                {props.employees.map(employee => <EmployeeDetail />)}
+                {props.employees.map(employee =>
+                    <EmployeeDetail key={employee._id} employee={employee} />
+                )}
             </div>
+            <button onClick={() => Meteor.subscribe('employees', 40) }
+                    className="btn btn-primary">Load More
+            </button>
+            <br/>
+            <br/>
         </div>
     )
 };
 
 export default createContainer(() => {
     // Set up subscriptions
-    Meteor.subscribe('employees');
+    Meteor.subscribe('employees', PER_PAGE);
 
     // Return object and send to EmployeeList as props
     return { employees: Employees.find({}).fetch() };
